fc27af8370c10087aa8179a7f15435ccf01d9250
lilizhaoUM <45952736+lilizhaoUM@users.noreply.github.com>
2019-01-02 14:09:19 -0500

    Update test example.R

--- test example.R
+++ test example.R
@@ -35,23 +35,15 @@ surv_test <- time[(n/2+1):n]
 cen_test =delta[(n/2+1):n]
 xs_test<- scale(x_test, center = mean, scale = std)
 
-weight=compute_weight(surv_train,cen_train,x_train)
-ypredw <- predictSurvprob(surv_train, cen_train, weight=weight, pickTime, xs_train, xs_test) 
+# get the risk probability for each pickTime
+ypredw <- predictSurvprob(surv_train, cen_train, weight=1, pickTime, xs_train, xs_test) 
 
-evaluateCondPseudo(1-ypredw, surv_train, cen_train, pickTime,
-                                surv_test, cen_test)
-#output
- [,1]      [,2]      [,3]       [,4]
-[1,] 0.7132203 0.6988963 0.7572581 0.09132486
-[2,] 0.7132196 0.6988963 0.7657678 0.15633887
-[3,] 0.7132196 0.6988963 0.7766325 0.21318703
-[4,] 0.7132196 0.6988963 0.7906950 0.25749522
-[5,] 0.7132196 0.6988963 0.8022966 0.27820046
-[5,] 0.7132196 0.6988963 0.8022966 0.27673515
+# obtain the c-index and Brier score
+evaluateCondPseudo(1-ypredw, surv_train, cen_train, pickTime, surv_test, cen_test)
+
 
 # IPCW calculation
 weight=compute_weight(surv_train,cen_train,x_train)
 ypredw <- predictSurvprob(surv_train, cen_train, weight=weight, pickTime, xs_train, xs_test) 
 
-evaluateCondPseudo(1-ypredw, surv_train, cen_train, pickTime,
-                                surv_test, cen_test)
+evaluateCondPseudo(1-ypredw, surv_train, cen_train, pickTime, surv_test, cen_test)
