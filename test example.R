ed569f7afb4b3b29b3cf97c613e9e58d6b3230d3
lilizhaoUM <45952736+lilizhaoUM@users.noreply.github.com>
2019-12-20 10:56:29 -0500

    Update test example.R

--- test example.R
+++ test example.R
@@ -8,10 +8,9 @@ library(survcomp)
 library(survAUC)
 
 # simulate survival data from a cox model with one covariate 
-# the censoring distribution depends on the covariate
-
 set.seed(2412)
 pickTime=c(0.7,1.7,3.2,5.3,8.3)
+n=2000
 x=rnorm(n,0,1)
 c0=0.1
 times=rexp(n,c0*exp(1*x))  
@@ -27,23 +26,43 @@ cen_train <- delta[1:(n/2)]
 # data normalization
 mean <- apply(as.matrix(x_train), 2, mean)
 std <- apply(as.matrix(x_train), 2, sd)
-xs_train <- scale(x_train, center = mean, scale = std)
+x_train <- scale(x_train, center = mean, scale = std)
 
 # data normalization
 x_test=x[(n/2+1):n]
 surv_test <- time[(n/2+1):n]
 cen_test =delta[(n/2+1):n]
-xs_test<- scale(x_test, center = mean, scale = std)
-
-# obtain the risk probability for each pickTime
-ypredw <- predictSurvprob(surv_train, cen_train, weight=1, pickTime, xs_train, xs_test) 
-
-# obtain the c-index (first column) and Brier score (last column)
-evaluateCondPseudo(1-ypredw, surv_train, cen_train, pickTime, surv_test, cen_test)
+x_test<- scale(x_test, center = mean, scale = std)
 
 
-# IPCW method
-weight=compute_weight(surv_train,cen_train,x_train)
-ypredw <- predictSurvprob(surv_train, cen_train, weight=weight, pickTime, xs_train, xs_test) 
+pseudoCond  = getPseudoConditional(surv_train, cen_train, pickTime)
+
+x <- x_train[pseudoCond$id,]
+
+# create dummy variables for the time points
+smatrix=model.matrix(~as.factor(pseudoCond$s)+0)
+x_train.all <- cbind(x, smatrix)
+
+y_train.all <- pseudoCond$pseudost
+
+model = pseudoDNN.train(x_train.all, y_train.all)
+
+
+# format the test data 
+x_test.all=do.call(rbind, replicate(length(pickTime), x_test, simplify=FALSE))
+
+s_test=rep(s,each=nrow(x_test))
+smatrix.test=model.matrix(~as.factor(s_test)+0)
+x_test.all=cbind(x_test.all,smatrix.test)
+
+# predict test data
+ypred.con <- pseudoDNN.predict(model, x_test.all)
+
+# format the conditional survival probability at  each time point
+ypred.con <- matrix(ypred.all, nrow=nrow(x_test))
+
+# obtain the marginal survival probability by multiple series of conditional probabilities
+ypred <- lapply(1:length(s), function(i) apply(ypred.con[,1:i, drop=FALSE], 1, prod))
+ypredsur <- Reduce(cbind, ypred)
 
 
