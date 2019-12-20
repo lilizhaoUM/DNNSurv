4ffb913b5c7e7db3f160b6325efc59bd2b4824c2
lilizhaoUM <45952736+lilizhaoUM@users.noreply.github.com>
2019-12-20 09:40:33 -0500

    Update Keras model.R

--- Keras model.R
+++ Keras model.R
@@ -1,18 +1,15 @@
-#----------------------------------------------------------------------------
-# A two-layer deep neural network model implemented in keras with default hyperparameters 
-# hyper-parameters in this model worked very well in simulation studies in the paper
-#----------------------------------------------------------------------------
+#----------------------------------------------------------------------------------------
+# Example of a neural network with two hidden layer implemented in R keras in the paper
+#-----------------------------------------------------------------------------------------
 pseudoDNN.train <- function(x_train, y_train){
   
   model <- keras_model_sequential() %>%
-    #use tanh instead of relu for activation function
     layer_dense(units = 8, kernel_regularizer = regularizer_l2(0.0001), activation = "tanh",
                 input_shape = dim(x_train)[[2]]) %>%
     layer_dense(units = 4, kernel_regularizer = regularizer_l2(0.01),
                 activation = "tanh") %>%
     layer_dense(units = 1, activation='sigmoid')
   
-  #use adam instead of rmsprop for optimizer
   model %>% compile(
     optimizer = optimizer_adam(lr = 0.0025),
     loss = "mse",
@@ -25,8 +22,30 @@ pseudoDNN.train <- function(x_train, y_train){
   model
 }
 
+#----------------------------------------------------------------------------------------
+# Another example of a neural network with one hidden layer implemented in R keras in the paper
+#-----------------------------------------------------------------------------------------
+pseudoDNN.train <- function(x_train, y_train){
+  # use selu instead of relu for some studies
+  model <- keras_model_sequential() %>%
+    layer_dense(units=16,  activation = "selu",bias_initializer = initializer_constant(0.0),
+                input_shape = dim(x_train.all)[[2]]) %>%
+    layer_dropout(rate = 0.2) %>%
+    layer_dense(units = 1, activation='sigmoid')
+  
+  model %>% compile(
+    optimizer = optimizer_rmsprop(lr = 0.001),
+    loss = "mse",
+    metrics = c("mae")
+  )
+  model %>% fit(x_train, y_train,
+                epochs = 1000, batch_size =256,
+                verbose = 0)
+  model
+}
+
 #----------------------------------------------------------------------------
-#prediction based on the above two-layer keras model
+#prediction based on the two-layer keras model
 #----------------------------------------------------------------------------
 pseudoDNN.predict <- function(model, x_test){
   ypred <- model %>% predict(x_test)
