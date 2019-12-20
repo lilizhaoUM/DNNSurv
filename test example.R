1f3bdd1e6b762bb9a7eb55cb31aa7f181eda423c
lilizhaoUM <45952736+lilizhaoUM@users.noreply.github.com>
2019-12-20 11:10:06 -0500

    Update test example.R

--- test example.R
+++ test example.R
@@ -34,13 +34,16 @@ surv_test <- time[(n/2+1):n]
 cen_test =delta[(n/2+1):n]
 x_test<- scale(x_test, center = mean, scale = std)
 
-
+# get the pseudo conditinal survival probability 
 pseudoCond  = getPseudoConditional(surv_train, cen_train, pickTime)
 
+# covaraite
 x <- x_train[pseudoCond$id,]
 
 # create dummy variables for the time points
 smatrix=model.matrix(~as.factor(pseudoCond$s)+0)
+
+#create input predictors 
 x_train.all <- cbind(x, smatrix)
 
 y_train.all <- pseudoCond$pseudost
@@ -50,7 +53,6 @@ model = pseudoDNN.train(x_train.all, y_train.all)
 
 # format the test data 
 x_test.all=do.call(rbind, replicate(length(pickTime), x_test, simplify=FALSE))
-
 s_test=rep(s,each=nrow(x_test))
 smatrix.test=model.matrix(~as.factor(s_test)+0)
 x_test.all=cbind(x_test.all,smatrix.test)
@@ -58,11 +60,9 @@ x_test.all=cbind(x_test.all,smatrix.test)
 # predict test data
 ypred.con <- pseudoDNN.predict(model, x_test.all)
 
-# format the conditional survival probability at  each time point
-ypred.con <- matrix(ypred.all, nrow=nrow(x_test))
-
 # obtain the marginal survival probability by multiple series of conditional probabilities
+ypred.con <- matrix(ypred.con, nrow=nrow(x_test))
 ypred <- lapply(1:length(s), function(i) apply(ypred.con[,1:i, drop=FALSE], 1, prod))
-ypredsur <- Reduce(cbind, ypred)
+surv_prob <- Reduce(cbind, ypred)
 
 
